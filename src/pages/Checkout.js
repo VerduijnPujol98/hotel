9ea52b16fab4d854ad2aeb2dc1853ad5f71832c3
VerduijnPujol98 <asukaleenverduijn@gmail.com>
2022-10-17 17:17:54 +0200

    Fixed Booked Dates

--- src/pages/Checkout.js
+++ src/pages/Checkout.js
@@ -1,7 +1,7 @@
-import { Box, Button, Card, Select, Stepper, Text, TextInput } from '@mantine/core';
+import { Box, Button, Card, MediaQuery, Select, Stepper, Text, TextInput } from '@mantine/core';
 import { IconBed, IconShieldCheck, IconUserCheck } from '@tabler/icons';
 import { addDoc, collection, collectionGroup, doc, getDoc, getDocs, onSnapshot, query, where } from 'firebase/firestore';
-import React, { useEffect, useState } from 'react'
+import React, { forwardRef, useEffect, useState } from 'react'
 import { db } from '../firebaseconfig';
 import eachDayOfInterval from 'date-fns/eachDayOfInterval';
 import ReactDatePicker from 'react-datepicker';
@@ -13,6 +13,8 @@ const Checkout = () => {
     const [ pushdoc, setPushDoc ] = useState("")
     const [ pushdate, setPushdate ] = useState("")
     const [ select, setSelect ] = useState("")
+    const [ selectcheckinDate, setSelectcheckinDate ] = useState(new Date())
+    const [ selectcheckoutDate, setSelectcheckoutDate ] = useState(new Date())
 
 
 
@@ -44,9 +46,26 @@ const Checkout = () => {
     }
 
 
+    const CheckinInput = forwardRef(({ value, onClick }, ref) => (
+        <TextInput 
+        ref={ref}
+        placeholder={value}
+        label = "Check In Date"
+        onClick={onClick}
+        >
+        </TextInput>
+      ));
 
+      const CheckoutInput = forwardRef(({ value, onClick }, ref) => (
+        <TextInput 
+        ref={ref}
+        placeholder={value}
+        label = "Check Out Date"
+        onClick={onClick}
+        >
+        </TextInput>
+      ));
 
-    
 
 
     useEffect(() => {
@@ -60,16 +79,16 @@ const Checkout = () => {
     
   return (
     <div style={{display: 'flex', justifyContent: 'center',}}>
+        <MediaQuery smallerThan="sm" styles={{ width: '80vw'}}>
         <Card shadow="sm" p="lg" radius="md" withBorder sx={{width: '50vw', marginTop: 50, overflow:'visible'}}>
         <Stepper active={active} onStepClick={setActive}>
-            <Stepper.Step icon={<IconUserCheck size={18}/>}>
+            <Stepper.Step icon={<IconBed size={18}/>}>
                 <Box sx={{display:'flex', marginTop: 20}}>
-
 
 
                     <Text color="dimmed" size='sm'>Your registration will be verified prior to your arrival.</Text>
                 </Box>
-                <Box sx={{display:'flex', justifyContent:'space-between', marginTop:20, flexDirection:'column'}}>
+                <Box sx={{display:'flex', marginTop:20}}>
                     <Select sx={{width:'47vw'}} 
                                 placeholder="Select a Room" 
                                 label="Select Room"
@@ -83,27 +102,48 @@ const Checkout = () => {
                 </Box>
                 <Box sx={{display:'flex' , flexDirection:'row', justifyContent:'space-between', marginTop:20}}>
                 <ReactDatePicker 
+                minDate={new Date()}
+                startDate={selectcheckinDate}
+                endDate={selectcheckoutDate}
+                selectsStart
+                selected={selectcheckinDate}
+                onChange={(date) => setSelectcheckinDate(date)}
+                customInput={<CheckinInput />}
                 excludeDateIntervals={Object.values(pushdate).map((data) => {
                                         return {start: (new Date(data.checkindate)), end: (new Date(data.checkoutdate))}
                                      })}
 
                 ></ReactDatePicker>
+
+                <ReactDatePicker 
+                startDate={selectcheckinDate}
+                endDate={selectcheckoutDate}
+                selectsEnd
+                selected={selectcheckoutDate}
+                onChange={(date) => setSelectcheckoutDate(date)}
+                customInput={<CheckoutInput />}
+                excludeDateIntervals={Object.values(pushdate).map((data) => {
+                                        return {start: (new Date(data.checkindate)), end: (new Date(data.checkoutdate))}
+                                     })}
+
+                ></ReactDatePicker>
+
                 </Box>
-                <Box sx={{display:'flex', justifyContent:'space-between', marginTop:20, flexDirection:'column'}}>
-                    <Button onClick={() => {}}>Test</Button>
-                </Box>
-                <Box sx={{display:'flex', justifyContent:'space-between', marginTop:20, flexDirection:'column'}}>
+
+                <Box sx={{display:'flex', marginTop:40, flexDirection:'column'}}>
                     <Button onClick={nextStep}>Next</Button>
                 </Box>
             </Stepper.Step>
-            <Stepper.Step icon={<IconBed size={18}/>}>
+            <Stepper.Step icon={<IconUserCheck size={18}/>}>
                 <Box sx={{display:'flex', marginTop: 20}}>
                     <Text color="dimmed" size='sm'>Your registration will be verified prior to your arrival.</Text>
                 </Box>
-                <Box sx={{display:'flex' , flexDirection:'row', justifyContent:'space-between', marginTop:20}}>
+                <MediaQuery smallerThan="sm" styles={{ display:'block', width: '30vw'}}>
+                <Box sx={{display:'flex' , flexDirection:'row', marginTop:20}}>
                     <TextInput sx={{width:'23vw'}} placeholder="Your First Name" label="First Name"></TextInput>
                     <TextInput sx={{width:'23vw'}} placeholder="Your Last Name" label="Last Name"></TextInput>
                 </Box>
+                </MediaQuery>
                 <Box sx={{display:'flex', justifyContent:'space-between', marginTop:40, flexDirection:'column'}}>
                     <TextInput sx={{width:'47vw'}} placeholder="Street Address" label="Address"></TextInput>
                     <TextInput sx={{width:'47vw', marginTop:20}} placeholder="Street Address Line 2"></TextInput>
@@ -115,12 +155,16 @@ const Checkout = () => {
                 <Box sx={{display:'flex', justifyContent:'space-between', marginTop:20, flexDirection:'column'}}>
                     <TextInput sx={{width:'47vw'}} placeholder="Zip/Postal Code"></TextInput>
                 </Box>
+                <Box sx={{display:'flex', marginTop:40, flexDirection:'column'}}>
+                    <Button onClick={nextStep}>Next</Button>
+                </Box>
             </Stepper.Step>
             <Stepper.Step icon={<IconShieldCheck size={18}/>}>
 
             </Stepper.Step>
         </Stepper>
         </Card>
+        </MediaQuery>
     </div>
   )
 }
